395f29d46f5e8c692c3f2117abac35c43a14a766
natitedros <tedros.natnael@gmail.com>
2024-06-26 23:06:52 -0400

    [fix] integration with react frontend completed

--- src/main/java/com/springbackend/blogbackend/rest/BlogRestController.java
+++ src/main/java/com/springbackend/blogbackend/rest/BlogRestController.java
@@ -4,6 +4,7 @@ package com.springbackend.blogbackend.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.springbackend.blogbackend.entity.Blog;
 import com.springbackend.blogbackend.service.BlogService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class BlogRestController {
     
